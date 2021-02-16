de9e61b09c4fc7a8cc22feb949e652bbe45d8b40
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-17 08:36:11 +0900

    upto what i did yesterday

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,51 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    # raise
+  end
+
+  def new
+    @task = Task.new
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def check
+    @task = Task.find(params[:id])
+    if @task.completed
+      @task.completed = false
+    else
+      @task.completed = true
+    end
+    @task.save
+    # raise
+    redirect_to tasks_path
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # to index all the tasks (read all)
   get 'tasks', to: "tasks#index", as: :tasks
+
+  # create one task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # read one task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  patch 'tasks/:id', to: 'tasks#edit', as: :edit_task
+
+  # update
+  patch 'tasks/:id', to: 'tasks#check', as: :task_completed
+
+  # delete one task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
