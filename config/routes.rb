071d2783f0e8fe582547cb10e4577f0d16884a54
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-16 13:43:25 +0900

    shows right, it can look into the details

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # to index all the tasks (read all)
-  get '/tasks', to: "tasks#index", as: :tasks
+  get 'tasks', to: "tasks#index", as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
