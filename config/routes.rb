520e7f495d0bc9b3afecf20d7f2f1d6f8b3a9f0b
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-16 12:50:48 +0900

    now tasks are showing up on the browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'task/test'
+  # verb '/path', to: 'controller#action', as: :prefix
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # to index all the tasks (read all)
+  get '/tasks', to: "tasks#index", as: :tasks
 end
